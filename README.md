cc4cb0fce9fea10da8f54113e43f02f4855fa5b3
Anmol Gupta <anmolgupta520@gmail.com>
2021-07-04 13:06:18 +0530

    refactored code; added demo

---
+++ README.md
@@ -0,0 +1,26 @@
+# Clock
+
+I used TypeScript with React to create a Simple Clock and Stopwatch.
+
+## Demo
+
+[Live Demo](https://typescript-react-clock.netlify.app/)
+
+![img](demo/time.gif)
+
+## Run
+
+- Clone the Repo
+  ```bash
+  git clone https://github.com/ish-u/react-typescript-clock.git
+  cd react-typescript-clock/clock
+  ```
+- Install Node Modules
+  ```bash
+  npm install
+  ```
+- Run
+  ```bash
+  npm start
+  ```
+  The App will be live at `http://localhost:3000/`
--- clock/src/App.tsx
+++ clock/src/App.tsx
@@ -1,68 +1,124 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, FC } from "react";
 import { Container, ButtonGroup, Button } from "react-bootstrap";
 import "./index.css";
 
-function App() {
-  const [hide, setHide] = useState<boolean>(true);
-  const [clock, setClock] = useState(new Date().toLocaleTimeString());
-  const [stopwatch, setStopwatch] = useState<number>(0);
-  const [stopwatchStatus, setTimerStatus] = useState(false);
+// A Interface for props of Clock and StopWatch Component
+interface DefaultProps {
+  show: boolean;
+}
+
+// Clock Component
+const Clock: FC<DefaultProps> = ({ show }): JSX.Element => {
+  const [clock, setClock] = useState<string>(new Date().toLocaleTimeString());
+
   useEffect(() => {
-    const t = setInterval(
+    const clock = setInterval(
       (): void => setClock(new Date().toLocaleTimeString()),
       1000
     );
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <>
+      {show && (
+        <div>
+          <h1 className="display-1" style={{ fontSize: "8rem" }}>
+            {clock}
+          </h1>
+        </div>
+      )}
+    </>
+  );
+};
+
+// StopWatch Component
+const StopWatch: FC<DefaultProps> = ({ show }): JSX.Element => {
+  const [stopwatch, setStopwatch] = useState<number>(0);
+  const [stopwatchStatus, setTimerStatus] = useState(false);
+  useEffect(() => {
     const timerInterval = setInterval((): void => {
       if (stopwatchStatus) {
         setStopwatch(stopwatch + 1);
       }
     }, 1000);
     return () => {
-      clearInterval(t);
       clearInterval(timerInterval);
     };
   }, [stopwatch, stopwatchStatus]);
+
+  // function to return formatted time in HH:MM:SS when given seconds
+  const returnFormattedTime = (seconds: number): string => {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+  };
+
   return (
-    <Container fluid className="clock">
+    <>
+      {!show && (
+        <div>
+          <h1 className="display-1" style={{ fontSize: "8rem" }}>
+            {returnFormattedTime(stopwatch)}
+          </h1>
+          <br></br>
+          <ButtonGroup>
+            <Button
+              style={{ border: "none" }}
+              variant="outline-dark"
+              onClick={() => setTimerStatus(!stopwatchStatus)}
+            >
+              {!stopwatchStatus ? "Start" : "Stop"}
+            </Button>
+            <Button
+              style={{ border: "none" }}
+              variant="outline-dark"
+              onClick={() => {
+                setTimerStatus(false);
+                setStopwatch(0);
+              }}
+            >
+              Reset
+            </Button>
+          </ButtonGroup>
+        </div>
+      )}
+    </>
+  );
+};
+
+function App() {
+  const [showClock, setShowClock] = useState<boolean>(true);
+
+  return (
+    <Container fluid className="main">
       <div className="group">
         <ButtonGroup>
           <Button
             variant="outline-dark"
-            // onClick={(): void => setHide(hide === false ? true : false)}
+            onClick={() => {
+              if (showClock === false) {
+                setShowClock(true);
+              }
+            }}
           >
-            <i className="bi bi-alarm"></i>
-          </Button>
-          <Button
-            variant="outline-dark"
-            // onClick={(): void => setHide(hide === true ? false : true)}
-          >
-            <i className="bi bi-clock-history"></i>
-          </Button>
-        </ButtonGroup>
-      </div>
-      <div className={!hide ? "hide" : ""}>
-        <h1 className="display-1">{clock}</h1>
-      </div>
-      <div className={hide ? "hide" : ""}>
-        <h1 className="display-1">{stopwatch}</h1>
-        <ButtonGroup>
-          <Button
-            variant="outline-dark"
-            onClick={() => setTimerStatus(!stopwatchStatus)}
-          >
-            {"Start"}
+            <i className="bi bi-clock"></i>
           </Button>
           <Button
             variant="outline-dark"
             onClick={() => {
-              setTimerStatus(false);
-              setStopwatch(0);
+              if (showClock === true) {
+                setShowClock(false);
+              }
             }}
           >
-            Reset
+            <i className="bi bi-stopwatch"></i>
           </Button>
         </ButtonGroup>
       </div>
+      <Clock show={showClock} />
+      <StopWatch show={showClock} />
     </Container>
   );
 }
